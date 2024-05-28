b805f2a1730aaeb192c1f63ab076e6b6c36fafa7
jukmifgguggh1337 <spamandjunkmail9@gmail.com>
2024-05-28 13:46:58 -0600

    coyote time
    also expanded level some more

--- Scripts/jlayer.gd
+++ Scripts/jlayer.gd
@@ -3,18 +3,23 @@ extends CharacterBody2D
 
 const SPEED = 120.0
 const JUMP_VELOCITY = -350.0
+const COYOTE_MAX = 9
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-var jump = false;
+var jump = false
+var coyote = 0
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+		if coyote > 0: coyote -= 1
+	else:
+		coyote = COYOTE_MAX
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("ui_accept") and (is_on_floor() or coyote > 0):
 		velocity.y = JUMP_VELOCITY
 		jump = true
 	
