6ceb35cd313189b3e52202b1d5f5438d49820c1e
jukmifgguggh1337 <spamandjunkmail9@gmail.com>
2024-05-28 15:17:57 -0600

    input map

--- Scripts/jlayer.gd
+++ Scripts/jlayer.gd
@@ -21,18 +21,18 @@ func _physics_process(delta):
 		coyote = COYOTE_MAX
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and (is_on_floor() or coyote > 0):
+	if Input.is_action_just_pressed("jump") and (is_on_floor() or coyote > 0):
 		velocity.y = JUMP_VELOCITY
 		jump = true
 	
 	# Handle early release jump cancel.
-	if jump and velocity.y < 0 and Input.is_action_just_released("ui_accept") and not is_on_floor():
+	if jump and velocity.y < 0 and Input.is_action_just_released("jump") and not is_on_floor():
 		velocity.y = 0
 		jump = false
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	var direction = Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
 		$Sprite.set_animation("Run")
