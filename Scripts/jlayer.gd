283f1aa407ce20b59f0a6ecfa9da22e604ddb137
jukmifgguggh1337 <spamandjunkmail9@gmail.com>
2024-05-28 13:10:09 -0600

    flip sprite depending on movement

--- Scripts/jlayer.gd
+++ Scripts/jlayer.gd
@@ -28,6 +28,10 @@ func _physics_process(delta):
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
+		if velocity.x < 0:
+			$Sprite.scale.x = -1
+		else:
+			$Sprite.scale.x = 1
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
