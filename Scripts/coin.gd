517c0276b807e1204378f01427938520e9e87851
Manpreet <87155707+ManpreetM1@users.noreply.github.com>
2024-06-02 15:34:39 -0600

    Added basic logic to coin

--- Scripts/coin.gd
+++ Scripts/coin.gd
@@ -1,6 +1,17 @@
 extends Area2D
 
 @onready var animation_player = $AnimationPlayer
+@onready var collect_coin = $CollectCoin
+
+var scene_path: String = "res://Scenes/coin.tscn"
 
 func _on_body_entered(body):
-	animation_player.play("pickup")
+	if body.name == "Jlayer" :
+		animation_player.play("pickup")
+		collect_coin.play()
+		var item = {
+			"name": "bytes",
+			"texture": Texture,
+			"scene_path": scene_path
+		}
+		Global.add_item(item)
