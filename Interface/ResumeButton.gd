421cc3e55a9df6c28f32a7eb0e1b89ee102ce7f2
JuanPCF1 <107865734+JuanPCF1@users.noreply.github.com>
2024-06-03 17:26:13 -0600

    Basic Pause Screen
    - ESC Brings up pause screen
    - Resume button works

---
+++ Interface/ResumeButton.gd
@@ -0,0 +1,14 @@
+extends Button
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	self.pressed.connect(self._button_pressed)
+	
+	
+func _button_pressed():
+	get_tree().paused = false
+	self.get_parent().get_parent().hide()
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
