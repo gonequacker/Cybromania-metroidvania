ae233513eb1400c2ce5f55620a91e863450b80a8
JuanPCF1 <107865734+JuanPCF1@users.noreply.github.com>
2024-06-06 11:26:07 -0600

    Game Over
    Game Over added with a resume button

--- Scripts/jlayer.gd
+++ Scripts/jlayer.gd
@@ -55,6 +55,7 @@ var weapon = 0 # currently held weapon.
 func _ready():
 	Global.set_player_reference(self) # tbh im not sure why im doing this
 	connect("hurt", get_parent().get_parent().get_node("CanvasLayer/HUD/Lives").life_changed)
+	connect("hurt", get_parent().get_parent().get_node("CanvasLayer/GameOver").life_changed)
 	connect("healed", get_parent().get_parent().get_node("CanvasLayer/HUD/Lives").life_changed)
 	emit_signal("healed", HEALTH_MAX)
 
