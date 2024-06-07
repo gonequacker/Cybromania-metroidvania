96cd2bf0dd0df278eaf9b04774c3bc8f367cd815
jukmifgguggh1337 <spamandjunkmail9@gmail.com>
2024-06-07 13:41:16 -0600

    removed UI print statements

--- Interface/Lives.gd
+++ Interface/Lives.gd
@@ -12,8 +12,6 @@ func _process(delta):
 func life_changed(player_health):
 	var lives_displayed = get_child_count()
 	while (player_health != lives_displayed):
-		print(player_health)
-		print(lives_displayed)
 		if lives_displayed > player_health:
 			get_children()[-1].queue_free()
 			lives_displayed-=1
