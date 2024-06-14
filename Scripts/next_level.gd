e182a1b237ce055b0a7d782c6af362b9049ba6fe
jukmifgguggh1337 <spamandjunkmail9@gmail.com>
2024-06-13 23:34:39 -0600

    next level portal opens after killing all enemies
    also fixed viewport size to be same as main branch

--- Scripts/next_level.gd
+++ Scripts/next_level.gd
@@ -1,12 +1,25 @@
 extends Area2D
 
+@onready var encryption_lock = $EncryptionLock
+
 @onready var fade_timer = $FadeTimer
 @onready var sfx = $SFX
+
+var enabled = false
 
 signal fade_out
 signal completed
 
+func _process(delta):
+	encryption_lock.visible = not enabled
+	var num_enemies_left = 0
+	var enemies_node = get_parent().get_node("Enemies")
+	if enemies_node: num_enemies_left = enemies_node.get_child_count()
+	if num_enemies_left <= 0:
+		enabled = true
+
 func _on_body_entered(body):
+	if not enabled: return
 	emit_signal("fade_out")
 	sfx.play()
 	Global.player_node.fade_to_black()
