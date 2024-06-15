1ab1d2da82a9975eea0eac9e9931212fdd501706
PJDuck <151961538+gonequacker@users.noreply.github.com>
2024-06-15 11:49:47 -0500

    added audio & more animation
    - added title screen music
    - added walk sequence for main character

--- Scenes/title_screen.gd
+++ Scenes/title_screen.gd
@@ -5,12 +5,25 @@ extends Control
 @onready var info_button = $MarginContainer/HBoxContainer/VBoxContainer/Info as Button
 @onready var quit_button = $MarginContainer/HBoxContainer/VBoxContainer/Quit as Button
 
+@onready var title_music = $AudioStreamPlayer2D
+@onready var title_animation = $AnimationPlayer
+@onready var player_sprite = $Walk_1/AnimatedSprite2D
+
 @onready var level_load = preload("res://Scenes/game.tscn")
+
 func _ready():
+	title_music.play()
 	start_button.button_down.connect(on_start_pressed)
 	quit_button.button_down.connect(on_quit_pressed)
-	$AnimationPlayer.play('new_animation')
+	title_animation.play("new_animation")
+	player_sprite.play('title_sprite_animation')
+	$fall_1/AnimatedSprite2D.play("fall_1")
+	$fall_2/AnimatedSprite2D.play("fall_2")
+	$fall_3/AnimatedSprite2D.play("fall_3")
+	$fall_4/AnimatedSprite2D.play("fall_4")
 	
+
+
 func on_start_pressed() -> void:
 	get_tree().change_scene_to_packed(level_load)
 	
@@ -18,5 +31,5 @@ func on_quit_pressed() -> void:
 	get_tree().quit()	
 	
 		
-
-
+func _on_animation_player_2_animation_changed(old_name, new_name):
+	$Sprite2D.hide()
