082a68c77ba99520dad5d8d71573f268c30bc2ea
Nathaniel Dafoe <ndafoe60@gmail.com>
2024-06-16 13:00:22 -0600

    added level 6 content, currently set as the second for debug purposes, square movement platforms not working, need to add more enemies and a few cookies.

--- dynamic_fishing_hook.gd
+++ dynamic_fishing_hook.gd
@@ -9,25 +9,30 @@ extends Node2D
 @onready var hurtSFX = $Hurt
 @onready var dieSFX = $Die
 
+signal killed
+
 var player = null
+var target_y = null
 @export var speed = 50
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
+	# If there is a target set, move to target.
+	# This is used for moving in to position.
+	if target_y != null:
+		position.y += (target_y - position.y)/ speed
+		if abs(position.y - target_y) < 5:
+			# Release target once reached. 
+			target_y = null
+	
+	# Regular behavior, chase the player when player eneters detection area. 
 	if player:
-		#print("there is a player detected! Moving y coor")
-		#print("player y: ", player.position.y)
-		#print("this position y: ", position.y)
 		position.y += (player.position.y - position.y)/ speed
-		#print("new position y: ", position.y)
 
 
 func _on_player_detection_body_entered(body):
 	player = body
-	#print("body entered: ", body)
 
 func _on_player_detection_body_exited(body):
-	#print("player exited: ", player == body)
 	player = null
 
 
@@ -39,6 +44,7 @@ func _on_hitbox_component_hurt():
 func _on_hitbox_component_killed():
 	anim.play("killed")
 	dieSFX.play()
+	emit_signal("killed")
 
 func _on_die_finished():
 	queue_free()
